dae6afed336ea92527449bad48cb26d1820c4830
ppoliani <ppoliani@gmail.com>
2022-09-06 13:21:59 +0300

    feat: implement the canva/auth endpoint that redirect to the dapp

--- src/api/src/endpoints/canva/auth.rs
+++ src/api/src/endpoints/canva/auth.rs
@@ -1,13 +1,23 @@
+use serde::{Deserialize};
 use actix_web::{
-  web::{Data},
+  web::{Data, Query},
   HttpResponse,
 };
 use crate::{
   utils::store::Store,
 };
 
-pub async fn exec(store: Data<Store>) -> HttpResponse {
-  let redirect_url = format!("{}/canva/auth", store.config.ticketland_dapp);
+#[derive(Deserialize, Clone)]
+pub struct QueryString {
+  user: String,
+  state: String,
+}
+
+pub async fn exec(
+  store: Data<Store>,
+  qs: Query<QueryString>,
+) -> HttpResponse {
+  let redirect_url = format!("{}/canva/auth?user={},state={}", store.config.ticketland_dapp, qs.user, qs.state);
 
   HttpResponse::Found()
     .append_header(("Location", redirect_url.as_str()))
--- src/api/src/endpoints/canva/config.rs
+++ src/api/src/endpoints/canva/config.rs
@@ -37,11 +37,10 @@ pub fn config(authn_middleware: Rc<AuthnMiddlewareFactory>, canva_key: String) -
       .wrap(Rc::clone(&canva_middleware_factory))
       .route(web::post().to(publish::exec))
     );
-
-    // This is the endpoint Canva will use to check if a user is authenticated
+    
     cfg.service(
       web::resource("auth")
-      .wrap(Rc::clone(&canva_middleware_factory))
+      // .wrap(Rc::clone(&canva_middleware_factory))
       .route(web::get().to(auth::exec))
     );
   }
