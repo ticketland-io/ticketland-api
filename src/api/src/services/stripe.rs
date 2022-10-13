4cfb56b2e4b819a13dc948e85fd5ea9b523e2d2a
ppoliani <ppoliani@gmail.com>
2022-10-13 22:24:14 +0300

    feat: create a stripe account and then create a link

--- src/api/src/services/stripe.rs
+++ src/api/src/services/stripe.rs
@@ -1,10 +1,14 @@
-use std::sync::Arc;
+use std::{
+  sync::Arc,
+  str::FromStr,
+};
 use futures_util::TryFutureExt;
 use serde::{Serialize};
 use stripe::{
   Account, AccountLink, AccountLinkType, AccountType, Client, CreateAccount,
   CreateAccountCapabilities, CreateAccountCapabilitiesCardPayments,
   CreateAccountCapabilitiesTransfers, CreateAccountLink,
+  AccountId,
 };
 use common_data::{
   helpers::{send_read, send_write},
@@ -37,23 +41,34 @@ pub async fn create_link(store: Arc<Store>, uid: String) -> Result<String, Error
       async move {
         // if no account link exist we would need to create one using Stripe API
         if result.0.len() > 0 {
-          Ok((false, TryInto::<StripeAccount>::try_into(result).unwrap().account_link.clone()))
+          Ok((false, AccountId::default(), TryInto::<StripeAccount>::try_into(result).unwrap().account_link.clone()))
         } else {
-          create_stripe_account_link(
-            store.config.stripe_key.clone(),
-            uid_copy.clone(),
-            ticketland_dapp,
-          )
+          create_stripe_account(store.config.stripe_key.clone())
+          .and_then(|account| {
+            async move {
+              create_stripe_account_link(
+                store.config.stripe_key.clone(),
+                account.id.clone(),
+                uid_copy.clone(),
+                ticketland_dapp,
+              )
+              .await
+              .map(|account_link| (true, account.id.clone(), account_link.url))
+            }
+          })
           .await
-          .map(|account_link| (true, account_link.url))
         }
       }
     })
-    .and_then(|(should_store, account_link)| {
+    .and_then(|(should_store, stripe_uid, account_link)| {
       async move {
         if should_store {
           // We need to store the newly created 
-          let (query, db_query_params) = upsert_account_link(uid.clone(), account_link.clone());
+          let (query, db_query_params) = upsert_account_link(
+            uid.clone(),
+            stripe_uid.to_string(),
+            account_link.clone()
+          );
       
           send_write(
             Arc::clone(&neo4j),
@@ -72,37 +87,52 @@ pub async fn create_link(store: Arc<Store>, uid: String) -> Result<String, Error
 
 pub async fn refresh_link(store: Arc<Store>, uid: String) -> Result<String, Error> {
   let neo4j = Arc::clone(&store.neo4j);
+  let (query, db_query_params) = read_stripe_user(uid.clone());
   let ticketland_dapp = store.config.ticketland_dapp.clone();
   let uid_copy = uid.clone();
 
-  create_stripe_account_link(
-    store.config.stripe_key.clone(),
-    uid_copy.clone(),
-    ticketland_dapp,
-  )
-  .and_then(|account_link| {
-    async move {
-      let (query, db_query_params) = upsert_account_link(uid.clone(), account_link.url.clone());
-      
-      send_write(
-        Arc::clone(&neo4j),
-        query,
-        db_query_params,
-      )
-      .await
-      .map(|_| account_link.url.clone())
-    }
-  })
-  .await
+  send_read(
+    Arc::clone(&neo4j),
+      query,
+      db_query_params,
+    )
+    .and_then(|result| {
+      async move {
+        let account = TryInto::<StripeAccount>::try_into(result).unwrap();
+
+        create_stripe_account_link(
+          store.config.stripe_key.clone(),
+          AccountId::from_str(&account.stripe_uid.clone()).unwrap(),
+          uid_copy.clone(),
+          ticketland_dapp,
+        )
+        .and_then(|account_link| {
+          async move {
+            let (query, db_query_params) = upsert_account_link(
+              uid.clone(),
+              account.stripe_uid.clone(),
+              account_link.url.clone()
+            );
+            
+            send_write(
+              Arc::clone(&neo4j),
+              query,
+              db_query_params,
+            )
+            .await
+            .map(|_| account_link.url.clone())
+          }
+        })
+        .await
+      }
+    })
+    .await
 }
 
-pub async fn create_stripe_account_link(
-  secret_key: String,
-  uid: String,
-  ticketland_dapp: String,
-) -> Result<AccountLink, Error> {
+pub async fn create_stripe_account(secret_key: String,) -> Result<Account, Error>  {
   let client = Client::new(secret_key);
-  let account = Account::create(
+  
+  Account::create(
     &client,
     CreateAccount {
       type_: Some(AccountType::Express),
@@ -117,12 +147,21 @@ pub async fn create_stripe_account_link(
     },
   )
   .await
-  .map_err(|error| Into::<Error>::into(format!("Stripe Error {:?}", error).as_str()))?;
+  .map_err(|error| Into::<Error>::into(format!("Stripe Error {:?}", error).as_str()))
+}
+
+pub async fn create_stripe_account_link(
+  secret_key: String,
+  stripe_uid: AccountId,
+  uid: String,
+  ticketland_dapp: String,
+) -> Result<AccountLink, Error> {
+  let client = Client::new(secret_key);
 
   AccountLink::create(
     &client,
     CreateAccountLink {
-        account: account.id.clone(),
+        account: stripe_uid,
         type_: AccountLinkType::AccountOnboarding,
         collect: None,
         expand: &[],
