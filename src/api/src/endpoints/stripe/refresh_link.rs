4aadbfbbf50a6fca5d23d782fca7f2c3dfd6b806
ppoliani <ppoliani@gmail.com>
2022-10-13 21:02:08 +0300

    feat: implement the refresh url endpoint

--- src/api/src/endpoints/stripe/refresh_link.rs
+++ src/api/src/endpoints/stripe/refresh_link.rs
@@ -8,7 +8,6 @@ use ticketland_core::{
   error::Error,
 };
 use api_helpers::{
-  middleware::auth::AuthData,
   services::{
     http::internal_server_error,
   },
@@ -17,7 +16,7 @@ use crate::{
   utils::store::Store,
   services::stripe::{
     Response,
-    create_link,
+    refresh_link,
   },
 };
 
@@ -28,10 +27,9 @@ pub struct Params {
 
 pub async fn exec(
   store: Data<Store>,
-  auth: AuthData,
   params: Path<Params>,
 ) -> HttpResponse {
-  create_link(Arc::clone(&store), auth.user.local_id.clone())
+  refresh_link(Arc::clone(&store), params.uid.clone())
   .await
   .map(|link| HttpResponse::Ok().json(Response {link}))
   .unwrap_or_else(|error: Error| internal_server_error(Some(error)))
