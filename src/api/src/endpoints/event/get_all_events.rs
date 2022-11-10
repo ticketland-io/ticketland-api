c54c8c23782e0d15ad350e36d1b9f45fe0fc69cc
ppoliani <ppoliani@gmail.com>
2022-11-10 22:19:18 +0200

    fix: get all events

--- src/api/src/endpoints/event/get_all_events.rs
+++ src/api/src/endpoints/event/get_all_events.rs
@@ -1,13 +1,12 @@
 use serde::Serialize;
 use actix_web::{web, HttpResponse};
-use eyre::Result;
 use api_helpers::{
-  services::data::{QueryString, exec_basic_db_read_endpoint},
+  services::{
+    data::QueryString,
+    http::create_read_response,
+  }
 };
-use ticketland_core::error::Error;
-use crate::{
-  utils::store::Store,
-};
+use crate::utils::store::Store;
 
 #[derive(Serialize)]
 pub struct BaseResponse<T: Serialize> {
@@ -21,20 +20,12 @@ pub struct BaseResponse<T: Serialize> {
 pub async fn exec(
   store: web::Data<Store>,
   qs: web::Query<QueryString>,
-) -> Result<HttpResponse, Error> {
+) -> HttpResponse {
   let skip = qs.skip.unwrap_or(0);
   let limit = qs.limit.unwrap_or(100);
 
   let mut postgres = store.postgres.lock().unwrap();
-  let result = postgres.read_events(skip, limit).await?;
+  let result = postgres.read_events(skip, limit).await;
 
-  Ok(
-    HttpResponse::Ok()
-    .json(BaseResponse {
-      count: result.len(),
-      result,
-      skip: Some(skip),
-      limit: Some(limit),
-    })
-  )
+  create_read_response(result, skip, limit)
 }
