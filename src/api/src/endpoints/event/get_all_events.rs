1faf3bfba057774f4b910ad860390507c9ddd5f5
ppoliani <ppoliani@gmail.com>
2022-11-11 16:55:44 +0200

    fix: get all events

--- src/api/src/endpoints/event/get_all_events.rs
+++ src/api/src/endpoints/event/get_all_events.rs
@@ -27,5 +27,5 @@ pub async fn exec(
   let mut postgres = store.postgres.lock().unwrap();
   let result = postgres.read_events(skip, limit).await;
 
-  create_read_response(result, skip, limit)
+  create_read_response(result, qs.skip, qs.limit)
 }
