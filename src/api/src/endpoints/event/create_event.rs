f6282f14609dd2918709afeb5b61ea256720a0ac
ppoliani <ppoliani@gmail.com>
2022-11-11 16:55:11 +0200

    fix: store_event

--- src/api/src/endpoints/event/create_event.rs
+++ src/api/src/endpoints/event/create_event.rs
@@ -1,9 +1,10 @@
 use std::sync::Arc;
+use eyre::Result;
 use actix_web::{web, HttpResponse};
 use actix_multipart::Multipart;
+use ticketland_core::error::Error;
 use api_helpers::{
   middleware::auth::AuthData,
-  services::http::internal_server_error,
 };
 use crate::{
   utils::store::Store,
@@ -16,15 +17,15 @@ pub async fn exec(
   auth: AuthData,
   params: web::Path<EventParams>,
   payload: Multipart
-) -> HttpResponse {
+) -> Result<HttpResponse, Error> {
   let event_id = params.event_id.clone();
 
-  store_event(
+  let metadata = store_event(
     Arc::clone(&store),
     event_id,
     auth.user.local_id,
     payload,
-  ).await
-  .map(|metadata| HttpResponse::Ok().json(metadata))
-  .unwrap_or_else(|error| internal_server_error(Some(error.root_cause())))
+  ).await?;
+
+  Ok(HttpResponse::Ok().json(metadata))
 }
