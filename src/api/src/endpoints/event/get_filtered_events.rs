bd71216175eeb4e4c15b7581b2196c388be1f402
StefVei <stefveisak@gmail.com>
2022-12-18 23:19:43 +0200

    add filter events parameter

--- src/api/src/endpoints/event/get_filtered_events.rs
+++ src/api/src/endpoints/event/get_filtered_events.rs
@@ -19,8 +19,9 @@ use crate::{
 QueryString! {
   pub struct QueryString {
     pub category: Option<i16>,
-    pub priceRange: Option<[u32; 2]>,
-    pub date: Option<NaiveDateTime>,
+    pub price_range: Option<[u32; 2]>,
+    pub start_date: Option<i64>,
+    pub end_date: Option<i64>,
     pub search: Option<String>,
   }
 }
@@ -39,14 +40,30 @@ pub async fn exec(
 ) -> HttpResponse {
   let skip = qs.skip.unwrap_or(0);
   let limit = qs.limit.unwrap_or(100);
-  // TODO: add the correct default prop
   let category = qs.category;
-  let priceRange = qs.priceRange.unwrap_or([0,2000]);
-  let date = qs.date;
+  // TODO: add the correct default prop
+  let price_range = qs.price_range.unwrap_or([0,2000]);
+  let start_date = qs.start_date.map(|date|{
+    // TODO: remove unwrap
+    return NaiveDateTime::from_timestamp_opt(date, 0).context("invalid start_date").unwrap();
+  });
+  let end_date = qs.end_date.map(|date|{
+    // TODO: remove unwrap
+    return NaiveDateTime::from_timestamp_opt(date, 0).context("invalid start_date").unwrap();
+  });
+  
   let name = qs.search.clone();
 
   let mut postgres = store.postgres.lock().await;
-  let result = postgres.read_filtered_events(category, priceRange, date, name, skip, limit).await;
+  let result = postgres.read_filtered_events(
+    category,
+    price_range,
+    start_date,
+    end_date,
+    name,
+    skip,
+    limit
+  ).await;
 
   create_read_response(result, skip, limit)
 }
