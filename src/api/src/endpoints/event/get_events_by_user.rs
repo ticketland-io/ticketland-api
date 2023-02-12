86160d1be76334e657852d2b9bbca5205e488f58
George Niotis <geoniotis@yahoo.com>
2023-02-12 21:57:36 +0200

    feat: adding date query parameters

--- src/api/src/endpoints/event/get_events_by_user.rs
+++ src/api/src/endpoints/event/get_events_by_user.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize};
+use eyre::{ContextCompat};
 use actix_web::{
   web::{Data, Query},
   HttpResponse
@@ -15,11 +16,14 @@ use ticketland_core::error::Error;
 use crate::{
   utils::store::Store,
 };
+use chrono::NaiveDateTime;
 
 QueryString! {
-  pub struct QueryString {}
+  pub struct QueryString {
+    pub start_date_from: Option<i64>,
+    pub start_date_to: Option<i64>,
+  }
 }
-
 pub async fn exec(
   store: Data<Store>,
   qs: Query<QueryString>,
@@ -27,9 +31,17 @@ pub async fn exec(
 ) -> Result<HttpResponse, Error> {
   let skip = qs.skip.unwrap_or(0);
   let limit = qs.limit.unwrap_or(100);
+  let start_date_from = if let Some(date) = qs.start_date_from {
+    Some(NaiveDateTime::from_timestamp_opt(date, 0).context("invalid start_date_from")?)
+  } else { None };
+  let start_date_to = if let Some(date) = qs.start_date_to {
+    Some(NaiveDateTime::from_timestamp_opt(date, 0).context("invalid start_date_to")?)
+  } else { None };
   let mut postgres = store.pg_pool.connection().await?;
   let result = postgres.read_account_events(
     auth.user.local_id.clone(),
+    start_date_from,
+    start_date_to,
     skip,
     limit,
   ).await;
