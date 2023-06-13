026690398ddb8478f846c42fe766c00cb7191319
George Niotis <geoniotis@yahoo.com>
2023-06-13 16:15:20 +0300

    fix: code style

--- src/api/src/main.rs
+++ src/api/src/main.rs
@@ -35,11 +35,9 @@ use ticketland_utils::logger::{
 
 fn error_handler<B>(res: dev::ServiceResponse<B>) -> Result<ErrorHandlerResponse<B>> {
   match res.response().error() {
-    Some(inner)   => ConsoleLogger.error(&format!("{:?}", inner)),
-    None          => (),
+    Some(inner) => ConsoleLogger.error(&inner.to_string()),
+    None => (),
   }
-
-  ConsoleLogger.error(&format!("Error occurred on request {:?}", res.request()));
 
   Ok(ErrorHandlerResponse::Response(res.map_into_left_body()))
 }
