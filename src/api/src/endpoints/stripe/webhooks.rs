afcdbef21d104c7a4b3a43a3346917b6f450fb57
Antonis Poulakis <antwnic4@gmail.com>
2023-07-03 23:05:15 +0300

    chore: comment out webhook checkout logic

--- src/api/src/endpoints/stripe/webhooks.rs
+++ src/api/src/endpoints/stripe/webhooks.rs
@@ -1,28 +1,12 @@
-use std::{
-  borrow::Borrow,
-  str::FromStr,
-};
+use std::borrow::Borrow;
 use actix_web::{
   web::{Bytes, Data},
   HttpRequest, HttpResponse,
 };
 use stripe::{EventObject, EventType, Webhook};
-use chrono::{Duration};
-use eyre::{Result, Report, ContextCompat};
-use solana_sdk::{
-  pubkey::Pubkey,
-};
-use ticketland_data::models::{
-  ticket::Cnt,
-  ticket_onchain_account::TicketOnchainAccount,
-};
+use eyre::{Result, Report};
 use ticketland_core::error::Error;
 use api_helpers::services::http::get_header_value;
-use program_artifacts::ticket_nft::pda as ticket_nft_pda;
-use ticketland_core::async_helpers::timeout;
-use ticketland_event_handler::{
-  services::ticket_purchase::pending_ticket_key,
-};
 use crate::{
   utils::store::Store,
 };
@@ -55,11 +39,11 @@ pub async fn handle_webhook(
             handle_account_updated(&store, account).await?;
           }
         }
-        EventType::CheckoutSessionCompleted => {
-          if let EventObject::CheckoutSession(session) = event.data.object {
-            handle_checkout_session(&store, session).await?;
-          }
-        }
+        // EventType::CheckoutSessionCompleted => {
+        //   if let EventObject::CheckoutSession(session) = event.data.object {
+        //     handle_checkout_session(&store, session).await?;
+        //   }
+        // }
         _ =>  Err(Report::msg(format!("Unknown event encountered in webhook: {:?}", event_type)))?
       }
   } else {
@@ -90,112 +74,104 @@ async fn handle_account_updated(
   Ok(())
 }
 
-async fn handle_checkout_session(store: &Data<Store>, session: stripe::CheckoutSession) -> Result<()> {
-  let metadata = &session.metadata;
-  let sale_type = metadata.get("sale_type").context("seat_index unavailable")?;
+// async fn handle_checkout_session(store: &Data<Store>, session: stripe::CheckoutSession) -> Result<()> {
+//   let metadata = &session.metadata;
+//   let sale_type = metadata.get("sale_type").context("seat_index unavailable")?;
 
-  match sale_type.as_str() {
-    "primary" => handle_new_ticket_purchase(&store, session).await,
-    "secondary" => handle_fill_sell_listing(&store, session).await,
-    _ => Err(Report::msg("invalid sale type"))?,
-  }
-}
+//   match sale_type.as_str() {
+//     "primary" => handle_new_ticket_purchase(&store, session).await,
+//     "secondary" => handle_fill_listing(&store, session).await,
+//     _ => Err(Report::msg("invalid sale type"))?,
+//   }
+// }
 
-async fn handle_new_ticket_purchase(store: &Data<Store>, session: stripe::CheckoutSession) -> Result<()> {
-  let metadata = session.metadata;
-  let ticket_nft = metadata.get("ticket_nft").context("ticket_nft unavailable")?.to_string();
-  let ticket_matadata = ticket_nft_pda::ticket_metadata(
-    &store.config.ticket_nft_program_state,
-    &Pubkey::from_str(&ticket_nft)?,
-  ).0;
-  let event_id = metadata.get("event_id").context("event_id unavailable")?;
-  let name = metadata.get("name").context("name unavailable")?;
-  let redis_key = pending_ticket_key(&event_id, &ticket_nft);
+// async fn handle_new_ticket_purchase(store: &Data<Store>, session: stripe::CheckoutSession) -> Result<()> {
+//   let metadata = session.metadata;
+//   let seat_index = metadata.get("seat_index").context("seat_index unavailable")?.to_string();
+//   let event_id = metadata.get("event_id").context("event_id unavailable")?;
+//   let name = metadata.get("name").context("name unavailable")?;
+//   let redis_key = pending_ticket_key(&event_id, &seat_index.to_string());
 
-  // Acquire a lock again so we update the state in Redis and Neo4j before someone else
-  // tries to purchase the same ticket which the current user has already purchased via Stripe
-  let _lock = store.redlock.lock(ticket_nft.as_bytes(), Duration::seconds(10).num_milliseconds() as usize).await?;
-  let mut redis = store.redis_pool.connection().await?;
+//   // Acquire a lock again so we update the state in Redis and Neo4j before someone else
+//   // tries to purchase the same ticket which the current user has already purchased via Stripe
+//   let _lock = store.redlock.lock(redis_key.as_bytes(), Duration::seconds(10).num_milliseconds() as usize).await?;
+//   let mut redis = store.redis_pool.connection().await?;
 
-  let seat_index = metadata.get("seat_index").context("seat_index unavailable")?.to_string();
 
-  timeout(
-    Duration::seconds(10).num_milliseconds() as u64,
-    redis.set_ex(&redis_key, &seat_index, Duration::days(1).num_milliseconds() as usize),
-  ).await??;
+//   timeout(
+//     Duration::seconds(10).num_milliseconds() as u64,
+//     redis.set_ex(&redis_key, &seat_index, Duration::days(1).num_milliseconds() as usize),
+//   ).await??;
 
-  let buyer_uid = metadata.get("buyer_uid").context("buyer_uid unavailable")?.to_string();
-  let seat_name = metadata.get("seat_name").context("seat_name unavailable")?.to_string();
-  let ticket_type_index: i16 = metadata.get("ticket_type_index").context("ticket_type_index unavailable")?.parse()?;
+//   let buyer_uid = metadata.get("buyer_uid").context("buyer_uid unavailable")?.to_string();
+//   let seat_name = metadata.get("seat_name").context("seat_name unavailable")?.to_string();
+//   let ticket_type_index: i16 = metadata.get("ticket_type_index").context("ticket_type_index unavailable")?.parse()?;
 
-  // Store the ticket nft in the db
-  let mut postgres = store.pg_pool.connection().await?;
+//   // // Store the ticket nft in the db
+//   let mut postgres = store.pg_pool.connection().await?;
 
-  let ticket_onchain_account = TicketOnchainAccount {
-    cnt_nft: ticket_nft.clone(),
-    ticket_metadata: ticket_matadata.to_string().clone(),
-  };
-  let ticket = Cnt {
-    cnt_nft: ticket_nft.clone(),
-    name: name.clone(),
-    event_id: event_id.clone(),
-    account_id: buyer_uid.clone(),
-    created_at: None,
-    ticket_type_index,
-    seat_name: seat_name.clone(),
-    seat_index: seat_index.parse::<i32>().unwrap(),
-    attended: false,
-    draft: false,
-  };
+//   let ticket = CNT {
+//     cnt_sui_address: None,
+//     event_id: event_id.clone(),
+//     account_id: buyer_uid.clone(),
+//     created_at: None,
+//     ticket_type_index,
+//     seat_name: seat_name.clone(),
+//     seat_index: seat_index.parse::<i32>().unwrap(),
+//     attended: false,
+//     draft: false,
+//   };
 
-  postgres.upsert_user_ticket(ticket, ticket_onchain_account).await?;
+//   postgres.upsert_user_cnt(ticket).await?;
 
-  // the ticket will ultimately be minted by another service that is handling these message
-  store.ticket_purchase_queue.new_ticket_purchase(
-    buyer_uid,
-    event_id.clone(),
-    metadata.get("sale_account").context("sale_account unavailable")?.to_string(),
-    ticket_nft,
-    metadata.get("recipient").context("recipient unavailable")?.to_string(),
-    seat_index,
-    seat_name,
-  ).await
-}
+//   // the ticket will ultimately be minted by another service that is handling these message
+//   store.ticket_purchase_queue.new_ticket_purchase(
+//     buyer_uid,
+//     event_id.clone(),
+//     metadata.get("recipient").context("recipient unavailable")?.to_string(),
+//     seat_index,
+//     seat_name,
+//     // TODO: this is missing the tx_bytes and signature
+//     "",
+//     "",
+//   ).await
+// }
 
-async fn handle_fill_sell_listing(store: &Data<Store>, session: stripe::CheckoutSession) -> Result<()> {
-  let metadata = session.metadata;
-  let ticket_nft = metadata.get("ticket_nft").context("ticket_nft unavailable")?.to_string();
-  let event_id = metadata.get("event_id").context("event_id unavailable")?;
-  let redis_key = pending_ticket_key(&event_id, &ticket_nft);
+// async fn handle_fill_listing(store: &Data<Store>, session: stripe::CheckoutSession) -> Result<()> {
+//   let metadata = session.metadata;
+//   let ticket_nft = metadata.get("ticket_nft").context("ticket_nft unavailable")?.to_string();
+//   let event_id = metadata.get("event_id").context("event_id unavailable")?;
+//   let redis_key = pending_ticket_key(&event_id, &ticket_nft);
 
-  // Acquire a lock again so we update the state in Redis and Neo4j before someone else
-  // tries to purchase the same ticket which the current user has already purchased via Stripe
-  let _lock = store.redlock.lock(ticket_nft.as_bytes(), Duration::seconds(10).num_milliseconds() as usize).await?;
-  let mut redis = store.redis_pool.connection().await?;
+//   // Acquire a lock again so we update the state in Redis and Neo4j before someone else
+//   // tries to purchase the same ticket which the current user has already purchased via Stripe
+//   let _lock = store.redlock.lock(ticket_nft.as_bytes(), Duration::seconds(10).num_milliseconds() as usize).await?;
+//   let mut redis = store.redis_pool.connection().await?;
 
-  let seat_index = metadata.get("seat_index").context("seat_index unavailable")?.to_string();
+//   let seat_index = metadata.get("seat_index").context("seat_index unavailable")?.to_string();
 
-  timeout(
-    Duration::seconds(10).num_milliseconds() as u64,
-    redis.set_ex(&redis_key, &seat_index, Duration::days(1).num_milliseconds() as usize),
-  ).await??;
+//   timeout(
+//     Duration::seconds(10).num_milliseconds() as u64,
+//     redis.set_ex(&redis_key, "1", Duration::days(1).num_milliseconds() as usize),
+//   ).await??;
 
-  let buyer_uid = metadata.get("buyer_id").context("buyer_id unavailable")?.to_string();
-  let sell_listing = metadata.get("sell_listing_account").context("sell_listing_account unavailable")?.to_string();
+//   let buyer_uid = metadata.get("buyer_id").context("buyer_id unavailable")?.to_string();
+//   let listing = metadata.get("listing_account").context("listing_account unavailable")?.to_string();
   
-  let mut postgres = store.pg_pool.connection().await?;
-  postgres.fill_sell_listing(
-    sell_listing.clone(),
-    ticket_nft.clone(),
-    buyer_uid.clone()
-  ).await?;
+//   let mut postgres = store.pg_pool.connection().await?;
+//   postgres.fill_listing(
+//     listing.clone(),
+//     ticket_nft.clone(),
+//     buyer_uid.clone()
+//   ).await?;
 
-  store.fill_sell_listing_queue.new_sell_listing(
-    buyer_uid,
-    event_id.clone(),
-    metadata.get("sale_account").context("sale_account unavailable")?.to_string(),
-    ticket_nft,
-    metadata.get("recipient").context("recipient unavailable")?.to_string(),
-    sell_listing,
-  ).await
-}
+//   // store.fill_listing_queue.new_listing(
+//   //   buyer_uid,
+//   //   event_id.clone(),
+//   //   metadata.get("sale_account").context("sale_account unavailable")?.to_string(),
+//   //   ticket_nft,
+//   //   metadata.get("recipient").context("recipient unavailable")?.to_string(),
+//   //   listing,
+//   // ).await
+//   Ok(())
+// }
