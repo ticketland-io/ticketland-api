da5cf59661cb72bda8ab55efd141a0c668aaaa46
Antonis Poulakis <antwnic4@gmail.com>
2023-07-16 08:15:55 +0300

    fix: get correct token price

--- src/api/src/services/price_feed.rs
+++ src/api/src/services/price_feed.rs
@@ -3,9 +3,9 @@ use eyre::Result;
 use price_feed::actors::price::get_price_key;
 use crate::utils::store::Store;
 
-pub async fn get_sol_price(store: Arc<Store>,) -> Result<i64> {
+pub async fn get_sui_price(store: Arc<Store>,) -> Result<i64> {
   let mut redis = store.redis_pool.connection().await?;
-  let price = redis.get(&get_price_key("solana"))
+  let price = redis.get(&get_price_key("sui"))
   .await?
   .parse::<i64>()?;
 
